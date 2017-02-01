7759b0de216a28da6d5f4d643717ab9c0e1825ed
slaffa715 <slaffa715@cis2.strose.edu>
2017-02-01 10:53:33 -0500

    added comment

---
+++ hello.java
@@ -0,0 +1,14 @@
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, Andrew");
+		System.out.println("hello professor");
+		// TODO Auto-generated method stub
+
+	}
+
+}
